8fd3a20e064fddb57454571577ee2dea37a341cb
deg5112 <deg5112@gmail.com>
2017-06-02 01:46:00 -0700

    the package list object is built dynamically after scraping/traversing the npm-depended repo

--- index.js
+++ index.js
@@ -1,41 +1,57 @@
 'use strict'
 const http = require('http');
+const https = require('https');
 const fs = require('fs');
 const getNpmTarballUrl = require('get-npm-tarball-url').default;
 const tar = require('tar-fs');
 const targz = require('tar.gz');
-var path = require('path');
+const cheerio = require('cheerio');
 
-const packages = {
-    lodash: '4.17.4',
-    request: '2.81.0',
-    async: '2.4.1',
-    express: '4.15.3',
-    chalk: '1.1.3',
-    bluebird: '3.5.0',
-    underscore: '1.8.3',
-    commander: '2.9.0',
-    debug: '2.6.8',
-    moment: '2.18.1'
-};
+//let me know if you want me to try anything else, looking forward to your feedback :)
 
 function downloadPackages (count, callback) {
-    for (let i in packages) {
-        var url = getNpmTarballUrl(i, packages[i]);
-        http.get(url, function(response) {
-            response.pipe(fs.createWriteStream('tmp/' + i + '.tar')).on('finish', function () {
-                targz().extract('tmp/'+i+'.tar', 'packages/'+i, function(err){
-                    if(err) {
-                        console.log('Something is wrong ', err.stack);
-                    }
-                    count -= 1;
-                    if (count === 0) {
-                        callback();
-                    }
-                });
-            });
+    let packages  = {};
+
+    https.get("https://www.npmjs.com/browse/depended", function(res) {
+        res.setEncoding('utf8');
+        let body = '';
+
+        res.on('data', function (chunk) {
+            body += chunk;
         });
-    }
+
+        res.on('end', function() {
+            const $ = cheerio.load(body);
+            let x = 0;
+
+            while (x < count) {
+                let packageName = $($('a.name')[x]).text();
+                let versionTargetSelector = '[href="/package/'+packageName+'"]';
+                packages[packageName] = $(versionTargetSelector).last().text();
+                x++;
+            }
+
+            for (let i in packages) {
+                let url = getNpmTarballUrl(i, packages[i]);
+                http.get(url, function(response) {
+                    response.pipe(fs.createWriteStream('tmp/' + i + '.tar')).on('finish', function () {
+                        targz().extract('tmp/'+i+'.tar', 'packages/'+i, function(err) {
+                            if (err) {
+                                console.log('Something is wrong ', err.stack);
+                            }
+
+                            count -= 1;
+                            if (count === 0) {
+                                callback();
+                            }
+                        });
+                    });
+                });
+            }
+        });
+    }).on('error', function(e) {
+        console.log("Got error: " + e.message)
+    });
 }
 
 module.exports = downloadPackages;
